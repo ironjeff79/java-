ca7c72f97f51e6342fac4413755e2676905bc37b
ironjeff79 <93599277+ironjeff79@users.noreply.github.com>
2023-04-14 21:19:56 +0900

    Update ArrayTest03.java

--- shuZu/ArrayTest03.java
+++ shuZu/ArrayTest03.java
@@ -17,18 +17,18 @@ import java.util.Arrays;
     二分法查找，排序  
 */
 public class ArrayTest03 {
-	public static void main(String[] args){
-		int[] arr = {3,6,5,12,1,2,32,5,5};
-	    //排序
-		Arrays.sort(arr);
-	    //输出
-	    for (int i = 0; i < arr.length; i++) {
-	    	System.out.println(arr[i]);
-	    }
-	     	
-	    //二分法查找
-	    int index = Arrays.binarySearch(arr,32);
-	    System.out.println(index == -1 ? "该元素不存在：" : "该元素下标是：" + index);
+    public static void main(String[] args){
+        int[] arr = {3,6,5,12,1,2,32,5,5};
+	//排序
+	Arrays.sort(arr);
+	//输出
+	for (int i = 0; i < arr.length; i++) {
+	    System.out.println(arr[i]);
 	}
+	     	
+	//二分法查找
+	int index = Arrays.binarySearch(arr,32);
+	System.out.println(index == -1 ? "该元素不存在：" : "该元素下标是：" + index);
+   }   
 
 }
