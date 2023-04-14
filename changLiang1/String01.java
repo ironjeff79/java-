6902949fc6c15dfc67cbe5d42b1dd9a94518cd69
ironjeff79 <93599277+ironjeff79@users.noreply.github.com>
2023-04-14 21:49:50 +0900

    Add files via upload

---
+++ changLiang1/String01.java
@@ -0,0 +1,49 @@
+package changLiang;
+/*运行结果
+false
+false
+true
+true
+true
+ */
+/*
+关于java JDK中内置的一个类: java.lang.String
+    1. String表示字符串类型，属于引用数据类型，不属于基本数据类型。
+    2. 在java中随便使用双引号括起来的都是String对象。例如"abc" "def" "HelloWorld"
+    3. java中规定，双引号括起来的字符串，是不可变的。
+    4. 在JDK当中双引号括起来的字符串，例如"abc"都是直接存储在"方法区"的"字符串常量池"当中的。（为何?因为字符串在实际的开发中使用太频繁，为了执行效率）
+    5. !!!注意：JDK新版本中"字符串常量池"被移动到"堆内存"中了
+*/
+public class String01{
+	public static void main(String[] args){
+		int i = 0; //i变量中保存的是100这个值
+        String s = "ABC"; //s变量中保存的是字符串对象在“字符串常量池”当中的内存地址。不是"ABC",而是0x1111。
+        
+        //这两行代码表示底层创建了3个字符串对象，都在字符串常量池当中
+        String s1 = "abcdef";
+        String s2 = "abcdef" + "xy";
+      
+        //分析：这是使用new的方式创建的字符串对象。这个代码中的"xy"是从哪里来的？凡是双引号括起来的，都在字符串常量池中有一份。
+        //注意!! new对象的时候，一定在堆内存中开辟空间
+        String s3 = new String("xy");
+        
+        //"Hello"是存储在方法区的字符串常量中 所以这个"Hello"不会新建（因为这个对象已经存在了）
+        String s4 = "Hello";
+        String s5 = "Hello";
+        //双等号比较的是变量中保存的内存地址。
+        System.out.println(s1 == s2);
+        
+        String x = new String("xyz");
+        String y = new String("xyz");
+        System.out.println(x == y);
+        
+        //通过这个案例的学习可以知道，字符串对象之间的比较不能用双等号，应该调用String类的equals方法。
+        //String类已经重写了equals方法，以下的equals方法调用的是String重写之后的equals
+        System.out.println(x.equals(y)); //true
+        
+        String k = new String("testString");
+        //"testString"这个字符串后面可以加"."  因为"testString"是一个String字符串对象。只要是对象都能调用方法。
+        System.out.println("testString".equals(k));  //建议使用这种方式，因为这个可以避免空指针异常。
+        System.out.println(k.equals("testString"));  //存在空指针异常的风险。      
+    }
+}
---
+++ changLiang1/String03.java
@@ -0,0 +1,53 @@
+package changLiang;
+/*运行结果
+hello world!
+abc
+bc
+我是中国人
+中国人
+HelloWorld!
+ 
+*/
+/*
+关于String类中的构造方法
+    1. String s = new String("");
+    2. String s = " ";  最常用
+    3. String s = new String(char数组);
+    4. String s = new String(char数组,起始下标,长度);
+    5. String s = new String(byte数组);
+    6. String s = new String(byte数组,起始下标,长度);
+*/
+
+public class String03{
+	public static void main(String[] args){
+		//创建字符串对象最常用的一种方式
+        String s1 = "hello world!";
+        //s1这个变量中保存的是一个内存地址。按说应该输出一个地址
+        //但是输出的是一个字符串，说明String类已经重写了toString()方法。
+        System.out.println(s1); 
+       
+        //这里只掌握常用的构造方法
+        byte[] bytes = {97,98,99}; //97是a,98是b,99是c
+        String s2 = new String(bytes); 
+        //输出一个引用的时候，会自动调用toString()方法，默认Object的话，会自动输出对象的内存地址。
+        //通过输出结果我们得出一个结论：String类已经重写了toString()方法
+        //输出字符串对象的话，输出的不是对象的内存地址，而是字符串本身。
+        System.out.println(s2); //abc
+        
+        //String(字节数组,数组元素下标的起始位置,长度)
+        //将byte数组中的一部分转换成字符串。
+        String s3 = new String(bytes,1,2);
+        System.out.println(s3); //bc 
+        
+        //将char数组全部转换成字符串
+        char[] chars = {'我','是','中','国','人'};
+        String s4 = new String(chars);
+        System.out.println(s4);
+        //将char数组的一部分转换成字符串
+        String s5 = new String(chars,2,3);
+        System.out.println(s5);
+        
+        String s6 = new String("HelloWorld!");
+        System.out.println(s6);
+    }
+}
